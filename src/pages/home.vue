6416499e35b98f0d57be4bf1f900035c91b645d6
Taras  Batyuchok <57962357+TarasBatyuchok@users.noreply.github.com>
2023-03-28 21:32:36 +0300

    add save data add input

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,6 +1,27 @@
 <template>
     <div>
       <h3 class="text-2xl font-bold text-left py-2">Product List</h3>
+      <div class="flex justify-center items-end space-x-2">
+        <div class="flex flex-col">
+        <label for="name">Name</label>
+        <input id="name" type="text" class="border border-gray-500 p-1" v-model="name">
+      </div>
+      <div class="flex flex-col">
+        <label for="">Color</label>
+        <input type="text" class="border border-gray-500 p-1" v-model="color">
+      </div>
+      <div class="flex flex-col ">
+        <label for="">Category</label>
+        <input type="text" class="border border-gray-500 p-1" v-model="category">
+      </div>
+      <div class="flex flex-col ">
+        <label for="">Price</label>
+        <input type="text" class="border border-gray-500 p-1" v-model="price">
+      </div>
+      <div>
+        <button class="bg-gray-600 hover:bg-slate-500 text-white p-1.5" @click="saveData()">Save</button>
+      </div>
+      </div>
       <div class="overflow-x-auto relative  sm:rounded-lg">
         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
           <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -41,21 +62,21 @@
             </tr>
           </thead>
           <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-              <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                Apple MacBook Pro 17"
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700" v-for="data in productyObj" :key="data.id">
+              <th class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {{ data.name }}
               </th>
               <td class="py-4 px-6">
-                Sliver
+                {{ data.color }}
               </td>
               <td class="py-4 px-6">
-                Laptop
+                {{ data.category }}
               </td>
               <td class="py-4 px-6">
-                $2999
+                ${{ data.price }}
               </td>
               <td class="py-4 px-6 text-right">
-                <a href="#" class="font-medium text-white p-2 bg-blue-500 hover:bg-blue-700 rounded-xl mx-1">Edit</a>
+                <a href="#" class="font-medium text-white p-2 bg-blue-500 hover:bg-blue-700 rounded-xl mx-1" @click="getData(data.id)">Edit</a>
                 <a href="#" class="font-medium text-white p-2 bg-red-500 hover:bg-red-700 rounded-xl">Delete</a>
               </td>
             </tr>
@@ -68,14 +89,62 @@
   
   <script>
   export default {
-    // data(){
-    //   return{
-    //     productyObj:{}
-    //   }
-    // },
-    // methods:{
+    data(){
+      return{
+        name:'',
+        color:'',
+        category: '',
+        price: 0,
 
-    // }
+        productyObj:[
+        {
+          id:1,
+          name:  "Apple MacBook Pro 17",
+          color: "silver",
+          category: "Laptop",
+          price: 2999
+        },
+        {
+          id:2,
+          name:  "Hp",
+          color: "black",
+          category: "Laptop",
+          price: 1500
+        },
+        {
+          id:3,
+          name:  "Xiomi",
+          color: "black",
+          category: "Laptop",
+          price: 3000
+        },
+        {
+          id:4,
+          name:  "Iphone 20",
+          color: "gold",
+          category: "Phone",
+          price: 800
+        },
+      ]
+      }
+    },
+    methods:{
+      getData(id){
+        alert(id)
+      },
+      saveData(){
+        const id = this.productyObj.length + 1
+        const data ={
+          id:id,
+          name: this.name,
+          color: this.color,
+          category: this.category,
+          price:this.price,
+        }
+        this.productyObj.push(data)
+        console.log(this.productyObj)
+      }
+    }
   
   }
   </script>
